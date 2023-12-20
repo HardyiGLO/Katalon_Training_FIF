100fb8e7dd476cc66bf73da21345933752dd391f
1878 <1878@LAPTOP-95N5CJET>
2023-12-20 17:01:58 +0700

    penambahan testsuit

---
+++ Test Suites/wiki-copy.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>wiki-copy</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d46f3d70-9c58-48ea-bc6f-5efb4aaba6e8</testSuiteGuid>
+   <testCaseLink>
+      <guid>8de754de-73d0-4d75-9078-02e0e5980dce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/wiki - Copy</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
