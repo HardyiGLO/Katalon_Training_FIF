428ca665559f470bc651ed8923c87f39c2af98f6
HardyiGLO <152849579+HardyiGLO@users.noreply.github.com>
2023-12-20 16:13:08 +0700

    Add files via upload

---
+++ Scripts/wiki/Script1703005528535.groovy
@@ -0,0 +1,34 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.wikipedia.org/')
+
+WebUI.setText(findTestObject('wikipedia/Page_Wikipedia/input_Search Wikipedia_search'), 'indonesia')
+
+WebUI.click(findTestObject('wikipedia/Page_Wikipedia/btnsearch'))
+
+//verify image location by xpath
+//The image comparison algorithm in Katalon Studio compares a screenshot of an object with the
+//displayed image of the corresponding web element on the active browser, pixel by pixel.
+//Therefore, if you capture object images using other tools, you have to resize the images to the
+//displayed sizes of the web elements on the active browser.
+WebUI.verifyImagePresent(findTestObject('wikipedia/Page_Indonesia - Wikipedia/img_Indonesian_mw-file-element'))
+
