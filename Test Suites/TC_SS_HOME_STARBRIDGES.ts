314b87489cc9d242e3a71f1014136a52f69124e6
1878 <1878@LAPTOP-95N5CJET>
2023-12-27 15:37:24 +0700

    aa

---
+++ Test Suites/TC_SS_HOME_STARBRIDGES.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC_SS_HOME_STARBRIDGES</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bdf9c9bd-f86a-4663-992d-4474a2c232eb</testSuiteGuid>
+   <testCaseLink>
+      <guid>8aed1906-7117-4154-abf3-c18e3c00e8ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestImageCompare</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
