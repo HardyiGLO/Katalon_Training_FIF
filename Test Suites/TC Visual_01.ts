931a0f41f0f2830d183c974d07272c1583ff4998
1878 <1878@LAPTOP-95N5CJET>
2023-12-27 17:19:59 +0700

    aa

--- Test Suites/TC Visual_01.ts
+++ Test Suites/TC Visual_01.ts
@@ -1,14 +1,21 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>TC Visual01</name>
+   <name>TC Visual_01</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>e25eddf7-4e2d-4bfe-a487-97aec9e8c95c</testSuiteGuid>
+   <testCaseLink>
+      <guid>39b921cb-a766-43f6-843a-58ff016925fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCheckUI</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
