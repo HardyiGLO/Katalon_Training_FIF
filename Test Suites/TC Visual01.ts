8893933f7947661662d4f0cad1a79e730541de69
1878 <1878@LAPTOP-95N5CJET>
2023-12-27 16:26:37 +0700

    test

---
+++ Test Suites/TC Visual01.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC Visual01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e25eddf7-4e2d-4bfe-a487-97aec9e8c95c</testSuiteGuid>
+</TestSuiteEntity>
