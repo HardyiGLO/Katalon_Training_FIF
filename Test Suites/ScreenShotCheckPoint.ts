89114ca3f61d552f84f593757302dda9133eb498
1878 <1878@LAPTOP-95N5CJET>
2023-12-29 16:47:16 +0700

    aa

---
+++ Test Suites/ScreenShotCheckPoint.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ScreenShotCheckPoint</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a5f9a4d0-351c-4010-ad80-b88d4a90cfd2</testSuiteGuid>
+   <testCaseLink>
+      <guid>c75e62ac-0a4b-4bbb-b379-c821f1689654</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC Campur/TakeScreenShoot/ScreenShotCheckPoint</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
