a16a16de7a92dda755227f99b096784a1d74a05a
Hardy <suhardi@indocyber.id>
2024-01-15 15:21:40 +0700

    aaa

---
+++ Test Suites/KRE/KRE Run Using Jenkins.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>KRE Run Using Jenkins</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>90de2aa2-9c37-42b5-9676-af889cf81824</testSuiteGuid>
+   <testCaseLink>
+      <guid>653b9831-b3b7-4b1a-b9b7-e6d51bc0f652</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Features KSE/KRE-DataBinding With SqlServer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8e9cf5d5-e1a2-4773-bc87-4fff6a59dea5</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c45908b5-57d2-477e-b01c-23d6c98aa815</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
